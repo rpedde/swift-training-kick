1b5ed8cf697fb1b45ca2b326e9c4b08802285c87
Ron Pedde <ron@pedde.com>
2011-05-20 11:07:12 -0500

    Kick from saved pvgrub image

--- kick.py
+++ kick.py
@@ -11,6 +11,7 @@ parser.add_option("-c", "--configfile", dest="configfile", default=None)
 parser.add_option("-u", "--username", dest="username", default=None)
 parser.add_option("-k", "--key", dest="key", default = None)
 parser.add_option("-n", "--number", dest="number", type="int", default=1)
+parser.add_option("-o", "--offset", dest="offset", type="int", default=1)
 
 (options,args) = parser.parse_args()
 config_hash = {}
@@ -36,13 +37,17 @@ except Exception:
 
 # find the image we want to load
 
-image = [ x for x in cs.images.list() if x.name.find("10.10") != -1 ][0]
+image = [ x for x in cs.images.list() if x.name.find("10.10-pvgrub") != -1 ][0]
 print 'Installing image "%s" (id: %d)' % (image.name, image.id)
 
 flavor = [ x for x in cs.flavors.list() if x.ram == 1024 ][0]
 print 'Installing on flavor "%s" (id: %d)' % (flavor.name, flavor.id)
 
-for x in range(1,options.number+1):
+if not flavor and not image:
+    print "Can't find image or flavor.  :("
+    sys.exit()
+
+for x in range(options.offset,options.offset + options.number):
     hostname = "training-%03d" % (x,)
     print "Kicking host %s" % (hostname, )
     
